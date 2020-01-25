d0552ac928f4cbdd5fb52b07def3fc747f67af53
Brandon Kalinowski <brandon@brandonkalinowski.com>
2020-01-25 10:44:06 +0000

    feat: runtypes

---
+++ runtypes.ts
@@ -0,0 +1,14 @@
+/**
+ * @author Tom Crockett
+ * @author Brandon Kalinowski
+ * @copyright 2020 Brandon Kalinowski
+ * @description A Deno port of the great RunTypes library. Use TypeScript in the runtime.
+ * Port introduces loose conversion. This means a check() call may modifies if required.
+ * Primitives should be assigned to themselves.
+ * "false" | "true" > boolean
+ * "null" > null
+ * "42" > 42
+ * See https://github.com/brandonkal/runtypes
+ */
+
+export * from 'https://raw.githubusercontent.com/brandonkal/runtypes/deno/src/index.ts'
