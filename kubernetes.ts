82d21198c28681b465f0ffc1c5d1acb610828cfa
Brandon Kalinowski <brandon@brandonkalinowski.com>
2020-01-25 10:36:34 +0000

    formatting

--- kubernetes.ts
+++ kubernetes.ts
@@ -2,6 +2,7 @@
  * @file **kubernetes.ts**
  * @author Brandon Kalinowski
  * @description Kubernetes Config Generation Library
+ * Refer to kite.ts for more info.
  */
 
 export * from './kubernetes/src/api.ts'
