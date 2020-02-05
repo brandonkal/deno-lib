cead4a39e4421fec2bf5b350a6f51145ad95b721
Brandon Kalinowski <brandon@brandonkalinowski.com>
2020-02-05 04:42:57 +0000

    template: exec and types

--- kite/template-cli.ts
+++ kite/template-cli.ts
@@ -1,26 +1,12 @@
-import * as flags from 'https://deno.land/std/flags/mod.ts'
-import template, { TemplateError } from './template.ts'
-
-const test = `\
----
-# urn:TerraformJSON:n=TerraformCode:4
-apiVersion: kite.run/v1alpha1
-kind: Terraform
-spec:
-  resource:
-    random_string:
-      test-p1:
-        length: 16
-      test-p2:
-        keepers:
-          amiResource: '{{ tf random_string.test-p1.result }}'
-        length: 40
----
-# urn:Resource:n=use-pass:3
-kind: password-reader
-length: 16
-password: '{{ tf random_string.test-p2.result }}'
-`
+import template, {
+	TemplateError,
+	TemplateConfigSpec,
+	TemplateConfig,
+	isTemplateConfig,
+	configFromSpec,
+} from './template.ts'
+import { getArgsObject } from '../args.ts'
+import { merge } from '../merge.ts'
 
 const helpText = `\
 Kite Template Tool by Brandon Kalinowski @brandonkal
@@ -46,7 +32,7 @@ The template operation is simple. It will:
      If you are using Kubernetes, you can then apply this config to your cluster using a tool like Kapp.
 
 Usage:
-  kapp [flags]
+  kite [flags]
 
 Flags:
   -q, --quiet         Set to disable logging progress to stderr unless an error is thrown
@@ -54,18 +40,63 @@ Flags:
   -h, --help          Prints this help message
 `
 
+// examples POST http kite.ts.com { exec: my-program-url.ts, name: dev, args: yamlObj }
+// example CLI:
+// kite -e my-program.ts -n dev -a - -r -q
+
+interface CliFlags extends TemplateConfigSpec {
+	e?: string
+	h?: boolean
+	config?: CliFlags
+	c?: CliFlags
+	n?: string
+	q?: boolean
+	p?: boolean
+	y?: string
+	/** shorthand to force reload of Terraform state */
+	r?: boolean
+	/** args without a flag. Contains yaml if exec is not specified. */
+	_?: string[]
+}
+
+export function canonicalizeOptions(opts: CliFlags): TemplateConfig {
+	if (opts.help || opts.h) {
+		console.log(helpText)
+		Deno.exit()
+	}
+	let nestedConfig = opts.config || opts.c
+	let nestedConfigCanonical
+	if (nestedConfig && !isTemplateConfig(nestedConfig)) {
+		nestedConfig = asConfig(nestedConfig)
+		nestedConfigCanonical = configFromSpec(nestedConfig)
+	}
+	const stdSpec = asConfig(opts)
+	const stdCfgCanonical = configFromSpec(stdSpec)
+	return merge(nestedConfigCanonical, stdCfgCanonical)
+}
+
+function asConfig(opts: CliFlags): TemplateConfigSpec {
+	const out = {
+		exec: opts.exec || opts.e,
+		reload: Boolean(opts.reload || opts.r),
+		quiet: Boolean(opts.quiet || opts.q),
+		name: opts.name || opts.n || undefined,
+		preview: Boolean(opts.preview),
+		allowEnvironment: Boolean(opts.allowEnvironment),
+	}
+	if (!out.exec) {
+		opts.yaml = opts.yaml || opts.y || opts._[0]
+	}
+	return out
+}
+
 /**
  * templateCli is the main CLI process for kite
- * @param args Takes the same arguments as the CLI
+ * @param cfg Takes canonicalizedOptions of the CLI
  */
-export default async function templateCli(args: Record<string, any>) {
+export default async function templateCli(cfg: TemplateConfig) {
 	try {
-		if (args.h || args.help) {
-			console.log(helpText)
-			Deno.exit()
-		}
-		const name = args.name || args.n || undefined
-		const out = await template(name, test, args)
+		const out = await template(cfg)
 		console.log(out.trimEnd())
 	} catch (err) {
 		if (err instanceof TemplateError) {
@@ -76,12 +107,21 @@ export default async function templateCli(args: Record<string, any>) {
 				throw err
 			} else {
 				console.error(err)
+				Deno.exit(1)
 			}
 		}
 	}
 }
 
+function d(it: any) {
+	console.log(JSON.stringify(it, undefined, 2))
+}
+
 if (import.meta.main) {
-	const args = flags.parse(Deno.args)
-	templateCli(args)
+	const args = getArgsObject(new Set(['args', 'a']))
+	d(args)
+	let canon = canonicalizeOptions(args)
+	d(canon)
+	Deno.exit(1)
+	templateCli(canon)
 }
