d6c83207c0c568a0e424768f35d4a3dbb31c2719
Brandon Kalinowski <brandon@brandonkalinowski.com>
2020-02-28 01:59:09 +0000

    feat: init karabiner lib

---
+++ karabiner.ts
@@ -0,0 +1,451 @@
+/**
+ * @file karabiner.ts
+ * @copyright 2020 Brandon Kalinowski (@brandonkal). All Rights reserved.
+ * @description A library and DSL to generate karabiner.json config. Can result in a 30x line reduction.
+ * 1. Call rule()
+ * 2. Call out()
+ */
+
+import * as keys from './karabiner/keys.ts'
+
+export const nil = undefined
+
+/** Represents all possible conditions or VarShorthand */
+export type ConditionOrShorthand = Condition | VarShorthand
+
+/**
+ * Generates a valid manipulator rule given a DSL definition. Can result in a 30x line reduction.
+ *
+ *
+ * Example:
+ * ```ts
+ * rule([t(VIMIUM, 1), t(G_TAPPED, 0)], ["caps_lock?", "g"], [t(G_TAPPED, 1)])
+ * //   ^ condition tests ^^^^^^^^^^^   ^optional mod ^key    ^ set variable
+ * ```
+ * @param conditions Specify conditions if required. Call test for variables or specify condition object.
+ * @param from Specify from as an array of string keycodes. If the string ends in "?", it is an optional modifier.
+ * If an object is passed as first parameter, it is interpretted as a IFromEvent.
+ * If multiple keys are specified that are not modifiers, simultaneous mode is assumed.
+ * @param to Specify to rules. Accepts IToEvent or or key name strings. Key strings cannot contain "?"
+ */
+export function rule(
+	conditions: ConditionOrShorthand[] | undefined,
+	from: keys.Key[] | [IFromEvent],
+	to: (keys.Key[] | IToEvent | VarShorthand)[],
+	others?: IManipulatorMod
+): IManipulator {
+	if (!Array.isArray(from) || !from.length) {
+		throw new Error(`from rule is required for ${from}`)
+	}
+	if (!Array.isArray(to) || !to.length) {
+		throw new Error(`to rule is required for ${to}`)
+	}
+	let outConditions: Condition[] = []
+	if (conditions) {
+		if (!Array.isArray(conditions)) {
+			throw new Error(`Conditions must be an array. Got ${conditions}`)
+		}
+		// All conditions must have a type
+		outConditions = conditions.map((condition) => {
+			if (isVarShorthand(condition)) {
+				return {
+					name: condition[0],
+					type: 'variable_if',
+					value: condition[1],
+				}
+			} else if (!condition.type) {
+				throw new Error(
+					`Condition is invalid. Expected [string, number] or valid type. Got ${condition}`
+				)
+			}
+			return condition
+		})
+	}
+	const outFrom =
+		typeof from[0] !== 'string' ? from[0] : genFromEvent(from as keys.Key[])
+	const outTo = to.map((key) => {
+		if (Array.isArray(key)) {
+			if (isVarShorthand(key)) {
+				return {
+					set_variable: {
+						name: key[0],
+						value: key[1],
+					},
+				}
+			} else {
+				return genFromEvent(key)
+			}
+		} else if (!key || typeof key !== 'object') {
+			throw new Error(`Invalid key ${key}`)
+		}
+		return key
+	})
+
+	return {
+		conditions: outConditions.length ? outConditions ?? undefined : undefined,
+		from: outFrom,
+		to: outTo.length === 1 ? outTo[0] : outTo,
+		type: 'basic',
+		...others,
+	}
+}
+
+/** A JSON replacer that sorts keys recursively and removes type key when required. */
+const replacer = (key: any, value: any) => {
+	if (value instanceof Object && !(value instanceof Array)) {
+		return Object.keys(value)
+			.sort()
+			.reduce((sorted, key) => {
+				;(sorted as any)[key] = value[key]
+				return sorted
+			}, {})
+	}
+	if (
+		Array.isArray(value) &&
+		(key === 'fn_function_keys' || key === 'simple_modifications')
+	) {
+		return value.map((val) => {
+			delete val.type
+			return val
+		})
+	}
+	return value
+}
+
+/**
+ * Call to output the config object as JSON.
+ * Call with `printf "%s" "$(deno generate.ts)" >| karabiner.json`
+ * @param config The object representing the karabiner.json config
+ */
+export function output(config: any) {
+	console.log(orderedJSONString(config))
+}
+
+///// =================== ///////
+///// ===   HELPERS   === ///////
+///// =================== ///////
+
+/** swap two keys.KeyCodes. Useful for windows keyboards */
+export function swap(a: keys.KeyCode, b: keys.KeyCode) {
+	return [Key.from(a).to(b), Key.from(b).to(a)]
+}
+
+/** repeat creates an array where the item is repeated count times.  */
+export function repeat<A>(item: A, count: number): A[] {
+	return Array.from<A>({ length: count }).fill(item)
+}
+
+/** Variable toggle shorthand function. Generates required variable_if or set_variable object. */
+export function v(name: string, value: 0 | 1): [string, 0 | 1] {
+	return [name, value]
+}
+
+/** VarShortShand is for creating a variable_if condition or a set_variable object */
+export type VarShorthand = [string, number]
+
+function genFromEvent(keyCodes: keys.Key[]): IFromEvent {
+	if (
+		!Array.isArray(keyCodes) ||
+		!keyCodes.length ||
+		keyCodes.some((key) => !keys.isValid(key))
+	) {
+		throw new Error(`Invalid from event ${keyCodes}`)
+	}
+	let out: IFromEvent = {}
+	const optionalMods: keys.KeyCode[] = []
+	const requiredMods: keys.KeyCode[] = []
+	const standardKeys: keys.KeyCode[] = []
+	keyCodes.forEach((key) => {
+		if (keys.isFromModifierKey(key)) {
+			if (key.endsWith('?')) {
+				optionalMods.push(key.replace('?', '') as keys.KeyCode)
+			} else {
+				requiredMods.push(key as keys.KeyCode)
+			}
+		} else {
+			standardKeys.push(key)
+		}
+	})
+	if (optionalMods.length || requiredMods.length) {
+		out.modifiers = {}
+		if (optionalMods.length) {
+			out.modifiers.optional = optionalMods
+		}
+		if (requiredMods.length) {
+			out.modifiers.mandatory = requiredMods
+		}
+	}
+	if (!standardKeys.length) {
+		// pop the last required modifier into key_code
+		if (out.modifiers?.mandatory?.length) {
+			const stdKey = out.modifiers.mandatory.pop() as keys.KeyCode
+			if (!out.modifiers.mandatory.length) {
+				out.modifiers.mandatory = undefined
+			}
+			out = { ...out, ...keyObject(stdKey) }
+		} else {
+			throw new Error(
+				`A from rule requires a non-optional key but got ${keyCodes}`
+			)
+		}
+	}
+	if (standardKeys.length > 1) {
+		out.simultaneous = standardKeys.map(keyObject)
+	} else if (standardKeys.length === 1) {
+		out = { ...out, ...keyObject(standardKeys[0]) }
+	}
+	return out
+}
+
+/** check if variable_if shorthand or variable_set shorthand */
+export function isVarShorthand(condition: any): condition is VarShorthand {
+	return (
+		Array.isArray(condition) &&
+		condition.length === 2 &&
+		typeof condition[0] === 'string' &&
+		typeof condition[1] === 'number'
+	)
+}
+
+/** print deterministic JSON string specifically for karabiner */
+function orderedJSONString(obj: any, space = 4) {
+	return JSON.stringify(obj, replacer, space).trimEnd()
+}
+
+function keyObject(code: keys.KeyCode) {
+	if (keys.isConsumerKey(code)) {
+		return {
+			consumer_key_code: code,
+		}
+	}
+	return {
+		key_code: code,
+	}
+}
+
+///// ================== ///////
+///// === INTERFACES === ///////
+///// ================== ///////
+
+export interface IRule {
+	description: string
+	manipulators: IManipulator[]
+}
+
+export interface IConditionApplication {
+	type: 'frontmost_application_if' | 'frontmost_application_unless'
+	/**
+	 * Bundle identifier regexs.
+	 * You can examine application's bundle identifier in EventViewer > Frontmost Application Tab
+	 */
+	bundle_identifiers?: string
+	/**
+	 * File path regexs.
+	 * You can examine application's file path in EventViewer > Frontmost Application Tab
+	 */
+	file_paths?: string
+	/** condition description for a human */
+	description?: string
+}
+
+export interface IConditionDevice {
+	type: 'device_if' | 'device_unless'
+	identifiers: {
+		vendor_id?: number | string
+		product_id?: number | string
+		location_id?: number | string
+		is_keyboard?: boolean
+		is_pointing_device?: boolean
+	}[]
+	/** condition description for a human */
+	description?: string
+}
+
+export interface IConditionKeyboardType {
+	type: 'keyboard_type_if' | 'keyboard_type_unless'
+	identifiers: ('ansi' | 'iso' | 'jis')[]
+	/** condition description for a human */
+	description?: string
+}
+
+export interface IConditionInputSource {
+	type: 'input_source_if' | 'input_source_unless'
+	identifiers: {
+		/** language regex */
+		language?: string
+		input_source_id?: number | string
+		input_mode_id?: number | string
+	}[]
+	/** condition description for a human */
+	description?: string
+}
+
+export interface IConditionVariable {
+	type: 'variable_if' | 'variable_unless'
+	name: string
+	value: number | string
+	/** condition description for a human */
+	description?: string
+}
+
+export interface IConditionEvent {
+	type: 'event_changed_if' | 'event_changed_unless'
+	value: boolean
+	/** condition description for a human */
+	description?: string
+}
+
+/** Represents all possible conditions */
+type Condition =
+	| IConditionApplication
+	| IConditionDevice
+	| IConditionEvent
+	| IConditionInputSource
+	| IConditionKeyboardType
+	| IConditionVariable
+
+export interface IManipulator {
+	description?: string
+	type: 'basic' | 'mouse_motion_to_scroll'
+	from: IFromEvent
+	to?: IToEvent[] | IToEvent
+	to_if_alone?: IToEvent[]
+	to_after_key_up?: IToEvent[]
+	to_if_held_down?: IToEvent[]
+	to_delayed_action?: IToEvent[]
+	conditions?: Condition[]
+	/** Override parametes suchs `to_delayed_action_delay_milliseconds` */
+	parameters?: any[]
+}
+
+/** represents a manipulator modification to be used by rule() function. */
+export interface IManipulatorMod {
+	description?: string
+	type?: 'mouse_motion_to_scroll'
+	to_if_alone?: IToEvent[]
+	to_after_key_up?: IToEvent[]
+	to_if_held_down?: IToEvent[]
+	to_delayed_action?: IToEvent[]
+	/** Override parametes suchs `to_delayed_action_delay_milliseconds` */
+	parameters?: any[]
+}
+
+export interface IMouseKey {
+	x?: number
+	y?: number
+	vertical_wheel?: number
+	horizontal_wheel?: number
+	speed_multiplier?: number
+}
+
+export interface IModifiers {
+	mandatory?: keys.KeyCode[]
+	optional?: keys.KeyCode[]
+}
+
+export interface ISumultaneousOptions {
+	/** Specify whether key_down detection is interrupted with unrelated events. */
+	detect_key_down_uninterruptedly?: boolean
+	/** Restriction of key_down order. */
+	key_down_order?: 'insensitive' | 'strict' | 'strict_inverse'
+	/** Restriction of key_up order. */
+	key_up_order?: 'insensitive' | 'strict' | 'strict_inverse'
+	/** When key_up events are posted. */
+	key_up_when?: 'any' | 'all'
+	/** events will be posted when all from events are released. */
+	to_after_key_up?: (IFromEvent | IToEvent)[]
+}
+
+export interface IFromEvent {
+	key_code?: keys.KeyCode
+	consumer_key_code?: keys.ConsumerKey
+	/**
+	 * The name of the mouse button i.e. button1
+	 * Be careful when using "any": "pointing_button" to avoid losing the left click button.
+	 */
+	pointing_button?: keys.KeyCode
+	/** Be careful when using "any": "pointing_button" to avoid losing the left click button. */
+	any?: 'key_code' | 'consumer_key_code' | 'pointing_button'
+	modifiers?: IModifiers
+	/**
+	 * manipulates keys which are pressed simultaneously in 50 milliseconds by default.
+	 * https://pqrs.org/osx/karabiner/json.html#simultaneous
+	 */
+	simultaneous?: IFromEvent[]
+	simultaneous_options?: ISumultaneousOptions[]
+}
+
+export interface IToEvent {
+	key_code?: keys.KeyCode
+	consumer_key_code?: keys.ConsumerKey
+	shell_command?: string
+	select_input_source?: any
+	set_variable?: {
+		name: string
+		value: number | string
+	}
+	mouse_keys?: IMouseKey
+	modifiers?: IModifiers
+	/** Defaults to false. If true, do not send key_down until another key is pressed */
+	lazy?: boolean
+	/** Defaults true */
+	repeat?: boolean
+	/**
+	 * The typical usage of halt is to cancel to_after_key_up if to_if_alone or to_if_held_down is triggered.
+	 * If "halt": true exists in `to_if_alone` or `to_if_held_down`,
+	 * the `to_after_key_up` is suppressed when `to_if_alone` or `to_if_held_down` is triggered.
+	 *
+	 * Defaults to true.
+	 */
+	halt?: boolean
+	/**
+	 *  An integer value. The default value is 0.
+	 * Specify a key press period for when both key down and key up events are posted at the same time (e.g. to_if_alone)
+	 * Generally hold_down_milliseconds is used with "key_code": "caps_lock".
+	 *
+	 * Defaults to 0
+	 */
+	hold_down_milliseconds?: number
+}
+
+//////////// Key Class //////////////
+
+/**
+ * A chainable class generator for shortening key map definitions
+ */
+export class Key {
+	private _from!: keys.KeyCode
+	private _to!: keys.KeyCode
+	constructor() {}
+	static from(code: keys.KeyCode) {
+		const that = new Key()
+		that._from = code
+		return that
+	}
+	to(code: keys.KeyCode) {
+		this._to = code
+		return this
+	}
+	protected toJSON() {
+		let out: any = {}
+		if (keys.isConsumerKey(this._from)) {
+			out.from = {
+				consumer_key_code: this._from,
+			}
+		} else {
+			out.from = {
+				key_code: this._from,
+			}
+		}
+		if (keys.isConsumerKey(this._to)) {
+			out.to = {
+				consumer_key_code: this._to,
+			}
+		} else {
+			out.to = {
+				key_code: this._to,
+			}
+		}
+		return out
+	}
+}
---
+++ karabiner/keys.ts
@@ -0,0 +1,388 @@
+/**
+ * @file karabiner/keys.ts
+ * @copyright 2020 Brandon Kalinowski (@brandonkal). All Rights reserved.
+ * @description Key definitions for Karabiner library.
+ */
+
+const consumerKeys = [
+	'display_brightness_increment',
+	'display_brightness_decrement',
+	'rewind',
+	'play_or_pause',
+	'fast_forward',
+	'mute',
+	'volume_increment',
+	'volume_decrement',
+	'eject',
+] as const
+/**
+ * List of consumer keys
+ */
+export type ConsumerKey = typeof consumerKeys[number]
+/**
+ * determines if a given string is a consumer-key
+ */
+export function isConsumerKey(x: string): x is ConsumerKey {
+	return consumerKeys.includes(x as ConsumerKey)
+}
+
+/**
+ * List of modifiers in the from definition
+ */
+const fromModifiers = [
+	'caps_lock',
+	'caps_lock?',
+	'left_command',
+	'left_command?',
+	'left_control',
+	'left_control?',
+	'left_option',
+	'left_option?',
+	'left_shift',
+	'left_shift?',
+	'right_command',
+	'right_command?',
+	'right_control',
+	'right_control?',
+	'right_option',
+	'right_option?',
+	'right_shift',
+	'right_shift?',
+	'fn',
+	'fn?',
+	'command',
+	'command?',
+	'control',
+	'control?',
+	'option',
+	'option?',
+	'shift',
+	'shift?',
+	'left_alt',
+	'left_alt?',
+	'left_gui',
+	'left_gui?',
+	'right_alt',
+	'right_alt?',
+	'right_gui',
+	'right_gui?',
+	'any',
+	'any?',
+] as const
+/**
+ * List of modifiers in the from definition
+ */
+export type FromModifier = typeof fromModifiers[number]
+/**
+ * determines if a given string is a from modifier key
+ */
+export function isFromModifierKey(x: string): x is FromModifier {
+	return fromModifiers.includes(x as FromModifier)
+}
+
+const toModifiers = [
+	'caps_lock',
+	'caps_lock?',
+	'left_command',
+	'left_command?',
+	'left_control',
+	'left_control?',
+	'left_option',
+	'left_option?',
+	'left_shift',
+	'left_shift?',
+	'right_command',
+	'right_command?',
+	'right_control',
+	'right_control?',
+	'right_option',
+	'right_option?',
+	'right_shift',
+	'right_shift?',
+	'fn',
+] as const
+/**
+ * List of modifiers in the to definition
+ */
+export type ToModifier = typeof toModifiers[number]
+/**
+ * determines if a given string is a to modifier
+ */
+export function isToModifierKey(x: string): x is ToModifier {
+	return toModifiers.includes(x as ToModifier)
+}
+
+/**
+ * Represents all standard posssible keycode labels and mouse button labels
+ */
+const keyCodes = [
+	'button1',
+	'button2',
+	'button3',
+	'button4',
+	'button5',
+	'button6',
+	'button7',
+	'button8',
+	'button9',
+	'button10',
+	'button11',
+	'button12',
+	'button13',
+	'button14',
+	'button15',
+	'button16',
+	'button17',
+	'button18',
+	'button19',
+	'button21',
+	'button22',
+	'button23',
+	'button24',
+	'button25',
+	'button26',
+	'button27',
+	'button28',
+	'button29',
+	'button30',
+	'button31',
+	'button32',
+	'any',
+	'command',
+	'shift',
+	'option',
+	'control',
+	'caps_lock',
+	'left_control',
+	'left_shift',
+	'left_option',
+	'left_command',
+	'right_control',
+	'right_shift',
+	'right_option',
+	'right_command',
+	'fn',
+	'return_or_enter',
+	'escape',
+	'delete_or_backspace',
+	'delete_forward',
+	'tab',
+	'spacebar',
+	'hyphen',
+	'equal_sign',
+	'open_bracket',
+	'close_bracket',
+	'backslash',
+	'non_us_pound',
+	'semicolon',
+	'quote',
+	'grave_accent_and_tilde',
+	'comma',
+	'period',
+	'slash',
+	'non_us_backslash',
+	'up_arrow',
+	'down_arrow',
+	'left_arrow',
+	'right_arrow',
+	'page_up',
+	'page_down',
+	'home',
+	'end',
+	'a',
+	'b',
+	'c',
+	'd',
+	'e',
+	'f',
+	'g',
+	'h',
+	'i',
+	'j',
+	'k',
+	'l',
+	'm',
+	'n',
+	'o',
+	'p',
+	'q',
+	'r',
+	's',
+	't',
+	'u',
+	'v',
+	'w',
+	'x',
+	'y',
+	'z',
+	'1',
+	'2',
+	'3',
+	'4',
+	'5',
+	'6',
+	'7',
+	'8',
+	'9',
+	'0',
+	'f1',
+	'f2',
+	'f3',
+	'f4',
+	'f5',
+	'f6',
+	'f7',
+	'f8',
+	'f9',
+	'f10',
+	'f11',
+	'f12',
+	'f13',
+	'f14',
+	'f15',
+	'f16',
+	'f17',
+	'f18',
+	'f19',
+	'f20',
+	'f21',
+	'f22',
+	'f23',
+	'f24',
+	'display_brightness_decrement',
+	'display_brightness_increment',
+	'mission_control',
+	'launchpad',
+	'dashboard',
+	'illumination_decrement',
+	'illumination_increment',
+	'rewind',
+	'play_or_pause',
+	'fastforward',
+	'mute',
+	'volume_decrement',
+	'volume_increment',
+	'eject',
+	'apple_display_brightness_decrement',
+	'apple_display_brightness_increment',
+	'apple_top_case_display_brightness_decrement',
+	'apple_top_case_display_brightness_increment',
+	'keypad_num_lock',
+	'keypad_slash',
+	'keypad_asterisk',
+	'keypad_hyphen',
+	'keypad_plus',
+	'keypad_enter',
+	'keypad_1',
+	'keypad_2',
+	'keypad_3',
+	'keypad_4',
+	'keypad_5',
+	'keypad_6',
+	'keypad_7',
+	'keypad_8',
+	'keypad_9',
+	'keypad_0',
+	'keypad_period',
+	'keypad_equal_sign',
+	'keypad_comma',
+	'vk_none',
+	'print_screen',
+	'scroll_lock',
+	'pause',
+	'insert',
+	'application',
+	'help',
+	'power',
+	'execute',
+	'menu',
+	'select',
+	'stop',
+	'again',
+	'undo',
+	'cut',
+	'copy',
+	'paste',
+	'find',
+	'international1',
+	'international2',
+	'international3',
+	'international4',
+	'international5',
+	'international6',
+	'international7',
+	'international8',
+	'international9',
+	'lang1',
+	'lang2',
+	'lang3',
+	'lang4',
+	'lang5',
+	'lang6',
+	'lang7',
+	'lang8',
+	'lang9',
+	'japanese_eisuu',
+	'japanese_kana',
+	'japanese_pc_nfer',
+	'japanese_pc_xfer',
+	'japanese_pc_katakana',
+	'keypad_equal_sign_as400',
+	'locking_caps_lock',
+	'locking_num_lock',
+	'locking_scroll_lock',
+	'alternate_erase',
+	'sys_req_or_attention',
+	'cancel',
+	'clear',
+	'prior',
+	'return',
+	'separator',
+	'out',
+	'oper',
+	'clear_or_again',
+	'cr_sel_or_props',
+	'ex_sel',
+	'left_alt',
+	'left_gui',
+	'right_alt',
+	'right_gui',
+	'vk_consumer_brightness_down',
+	'vk_consumer_brightness_up',
+	'vk_mission_control',
+	'vk_launchpad',
+	'vk_dashboard',
+	'vk_consumer_illumination_down',
+	'vk_consumer_illumination_up',
+	'vk_consumer_previous',
+	'vk_consumer_play',
+	'vk_consumer_next',
+	'volume_down',
+	'volume_up',
+] as const
+/**
+ * Represents all standard posssible keycode labels and mouse button labels
+ */
+export type KeyCode = typeof keyCodes[number]
+/**
+ * determines if a given string is a key code
+ */
+export function isKeyCode(x: string): x is KeyCode {
+	return keyCodes.includes(x as KeyCode)
+}
+
+/**
+ * Union of all valid keycodes
+ */
+export type Key = KeyCode | ToModifier | FromModifier
+/**
+ * Check if a valid key
+ */
+export function isValid(x: Key): x is Key {
+	return (
+		isKeyCode(x) ||
+		isFromModifierKey(x) ||
+		isToModifierKey(x) ||
+		isConsumerKey(x)
+	)
+}
