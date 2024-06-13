89e35063fd88c4f087921642c50b63686d9fc551
Brandon Kalinowski <brandon@brandonkalinowski.com>
2024-06-13 02:15:10 -0400

    lint comment

--- proxymise.ts
+++ proxymise.ts
@@ -11,50 +11,49 @@
  */
 
 export function proxymise(target: any) {
-	if (typeof target === 'object') {
-		const proxy = () => target
-		//@ts-ignore
-		proxy.__proxy__ = true
-		return new Proxy(proxy, handler)
+	if (typeof target === "object") {
+		const proxy = () => target;
+		//@ts-ignore: extending proto for library
+		proxy.__proxy__ = true;
+		return new Proxy(proxy, handler);
 	}
-	return typeof target === 'function' ? new Proxy(target, handler) : target
+	return typeof target === "function" ? new Proxy(target, handler) : target;
 }
 
 const handler = {
 	get(target: any, property: any, receiver: any): any {
-		if (target.__proxy__) target = target()
+		if (target.__proxy__) target = target();
 		if (
-			property !== 'then' &&
-			property !== 'catch' &&
-			typeof target.then === 'function'
+			property !== "then" &&
+			property !== "catch" &&
+			typeof target.then === "function"
 		) {
 			return proxymise(
-				target.then((value: any) => get(value, property, receiver))
-			)
+				target.then((value: any) => get(value, property, receiver)),
+			);
 		}
-		return proxymise(get(target, property, receiver))
+		return proxymise(get(target, property, receiver));
 	},
 
 	apply(target: any, thisArg: any, argumentsList: any): any {
-		if (target.__proxy__) target = target()
-		if (typeof target.then === 'function') {
+		if (target.__proxy__) target = target();
+		if (typeof target.then === "function") {
 			return proxymise(
 				target.then((value: any) =>
 					Reflect.apply(value, thisArg, argumentsList)
-				)
-			)
+				),
+			);
 		}
-		return proxymise(Reflect.apply(target, thisArg, argumentsList))
+		return proxymise(Reflect.apply(target, thisArg, argumentsList));
 	},
-}
+};
 
 const get = (target: any, property: any, receiver: any) => {
-	const value =
-		typeof target === 'object'
-			? Reflect.get(target, property, receiver)
-			: target[property]
-	if (typeof value === 'function' && typeof value.bind === 'function') {
-		return Object.assign(value.bind(target), value)
+	const value = typeof target === "object"
+		? Reflect.get(target, property, receiver)
+		: target[property];
+	if (typeof value === "function" && typeof value.bind === "function") {
+		return Object.assign(value.bind(target), value);
 	}
-	return value
-}
+	return value;
+};
